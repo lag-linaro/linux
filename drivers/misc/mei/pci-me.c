4aceb731cbdde7fee8a34746772bea1a3f6d0754
Alexander Usyskin <alexander.usyskin@intel.com>
2016-09-12 16:21:43 +0300

    mei: me: add kaby point device ids
    commit ac182e8abc6f93c1c4cc12f042af64c9d7be0d1e upstream.
    
    Add device ids for Intel Kabypoint PCH (Kabylake)
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I96d38c90bdfb6b6d60d691104439d2cb543bb1db

--- drivers/misc/mei/pci-me.c
+++ drivers/misc/mei/pci-me.c
@@ -83,6 +83,9 @@ static const struct pci_device_id mei_me_pci_tbl[] = {
 	{MEI_PCI_DEVICE(MEI_DEV_ID_WPT_LP, mei_me_pch_cfg)},
 	{MEI_PCI_DEVICE(MEI_DEV_ID_WPT_LP_2, mei_me_pch_cfg)},
 
+	{MEI_PCI_DEVICE(MEI_DEV_ID_KBP, mei_me_pch8_cfg)},
+	{MEI_PCI_DEVICE(MEI_DEV_ID_KBP_2, mei_me_pch8_cfg)},
+
 	/* required last entry */
 	{0, }
 };

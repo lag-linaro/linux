b715562aa7da82d026de197812ed274939698de9
Eric Dumazet <edumazet@google.com>
2016-09-20 18:06:17 -0700

    net: get rid of an signed integer overflow in ip_idents_reserve()
    commit adb03115f4590baa280ddc440a8eff08a6be0cb7 upstream.
    
    Jiri Pirko reported an UBSAN warning happening in ip_idents_reserve()
    
    [] UBSAN: Undefined behaviour in ./arch/x86/include/asm/atomic.h:156:11
    [] signed integer overflow:
    [] -2117905507 + -695755206 cannot be represented in type 'int'
    
    Since we do not have uatomic_add_return() yet, use atomic_cmpxchg()
    so that the arithmetics can be done using unsigned int.
    
    Fixes: 04ca6973f7c1 ("ip: make IP identifiers less predictable")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I86cee1094b1abeec798e954d7f8c5adc8e40a358

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -475,12 +475,18 @@ u32 ip_idents_reserve(u32 hash, int segs)
 	struct ip_ident_bucket *bucket = ip_idents + hash % IP_IDENTS_SZ;
 	u32 old = ACCESS_ONCE(bucket->stamp32);
 	u32 now = (u32)jiffies;
-	u32 delta = 0;
+	u32 new, delta = 0;
 
 	if (old != now && cmpxchg(&bucket->stamp32, old, now) == old)
 		delta = prandom_u32_max(now - old);
 
-	return atomic_add_return(segs + delta, &bucket->id) - segs;
+	/* Do not use atomic_add_return() as it makes UBSAN unhappy */
+	do {
+		old = (u32)atomic_read(p_id);
+		new = old + delta + segs;
+	} while (atomic_cmpxchg(p_id, old, new) != old);
+
+	return new - segs;
 }
 EXPORT_SYMBOL(ip_idents_reserve);
 

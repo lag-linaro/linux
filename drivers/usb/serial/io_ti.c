5bf1d93676b614e47387f175f491962a02053cba
Johan Hovold <johan@kernel.org>
2017-01-03 16:39:45 +0100

    USB: serial: io_ti: fix I/O after disconnect
    commit 2330d0a853da260d8a9834a70df448032b9ff623 upstream.
    
    Cancel the heartbeat work on driver unbind in order to avoid I/O after
    disconnect in case the port is held open.
    
    Note that the cancel in release() is still needed to stop the heartbeat
    after late probe errors.
    
    Fixes: 26c78daade0f ("USB: io_ti: Add heartbeat to keep idle EP/416 ports from disconnecting")
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I6a0a4570fab5bcfe8c0419fd4e1000cd4c3c192e

--- drivers/usb/serial/io_ti.c
+++ drivers/usb/serial/io_ti.c
@@ -2414,6 +2414,9 @@ static int edge_startup(struct usb_serial *serial)
 
 static void edge_disconnect(struct usb_serial *serial)
 {
+	struct edgeport_serial *edge_serial = usb_get_serial_data(serial);
+
+	cancel_delayed_work_sync(&edge_serial->heartbeat_work);
 }
 
 static void edge_release(struct usb_serial *serial)

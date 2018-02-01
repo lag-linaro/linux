fd4b5eff3ef792fc2addc0cbabe026dfc151a7ce
Connor O'Brien <connoro@google.com>
2018-01-31 18:11:57 -0800

    ANDROID: cpufreq: track per-task time in state
    Add time in state data to task structs, and create
    /proc/<pid>/time_in_state files to show how long each individual task
    has run at each frequency.
    Create a CONFIG_CPU_FREQ_TIMES option to enable/disable this tracking.
    
    [CPNOTE: 01/07/21] Lee: Requested status on the bug
    
    Bug: 72339335
    Bug: 127641090
    Test: Read /proc/<pid>/time_in_state
    Change-Id: Ia6456754f4cb1e83b2bc35efa8fbe9f8696febc8
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [adelva: Folded the following changes into this patch:
             a6d3de6a7fba ("ANDROID: Reduce use of #ifdef CONFIG_CPU_FREQ_TIMES")
             b89ada5d9c09 ("ANDROID: Fix massive cpufreq_times memory leaks")]
    Signed-off-by: Alistair Delva <adelva@google.com>
    [Lee: Squash in all subsequent changes]
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Lee Jones <joneslee@google.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -104,6 +104,7 @@
 #include <linux/rseq.h>
 #include <uapi/linux/pidfd.h>
 #include <linux/pidfs.h>
+#include <linux/cpufreq_times.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -588,6 +589,7 @@ void free_task(struct task_struct *tsk)
 #ifdef CONFIG_SECCOMP
 	WARN_ON_ONCE(tsk->seccomp.filter);
 #endif
+	cpufreq_task_times_exit(tsk);
 	release_user_cpus_ptr(tsk);
 	scs_release(tsk);
 
@@ -2238,6 +2240,8 @@ __latent_entropy struct task_struct *copy_process(
 	if (args->io_thread)
 		p->flags |= PF_IO_WORKER;
 
+	cpufreq_task_times_init(p);
+
 	if (args->name)
 		strscpy_pad(p->comm, args->name, sizeof(p->comm));
 
@@ -2802,6 +2806,8 @@ pid_t kernel_clone(struct kernel_clone_args *args)
 
 	if (IS_ERR(p))
 		return PTR_ERR(p);
+
+	cpufreq_task_times_alloc(p);
 
 	/*
 	 * Do this prior waking up the new thread - the thread pointer

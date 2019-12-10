0f46b01dd8f4ee50e9b6279109515752bf4310a0
Lee Jones <lee.jones@linaro.org>
2019-12-10 08:30:38 +0000

    fixup! iio: hmc5843: fix potential NULL pointer dereferences
    Change-Id: I56ec82cf3c193bf1e45ddd53b9b383201d31462b

--- drivers/staging/iio/magnetometer/hmc5843_i2c.c
+++ drivers/staging/iio/magnetometer/hmc5843_i2c.c
@@ -59,7 +59,7 @@ static struct regmap_config hmc5843_i2c_regmap_config = {
 static int hmc5843_i2c_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
-	struct regmap *regmap = devm_regmap_init_i2c(cli,
+	struct regmap *regmap = devm_regmap_init_i2c(client,
 			&hmc5843_i2c_regmap_config);
 	if (IS_ERR(regmap))
 		return PTR_ERR(regmap);

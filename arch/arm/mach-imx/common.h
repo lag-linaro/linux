8eef85316d34416828b8e6a8a61150ead07adc15
Lee Jones <lee.jones@linaro.org>
2020-03-25 10:18:04 +0000

    fixup! ARM: imx: build v7_cpu_resume() unconditionally
    Change-Id: I48439863a24ba01b76f929c6e3c69159c87537b1

--- arch/arm/mach-imx/common.h
+++ arch/arm/mach-imx/common.h
@@ -126,6 +126,8 @@ void imx6_suspend(void __iomem *ocram_vbase);
 static inline void imx6_suspend(void __iomem *ocram_vbase) {}
 #endif
 
+void v7_cpu_resume(void);
+
 void imx6q_pm_init(void);
 void imx6dl_pm_init(void);
 void imx6sl_pm_init(void);

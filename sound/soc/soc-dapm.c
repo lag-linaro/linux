812c0aa15be3a694326999a7e5c7e6c6e0f1dc48
Lee Jones <lee.jones@linaro.org>
2020-04-06 12:18:25 +0100

    Revert "ASoC: dapm: Make sure we have a card when displaying component widgets"
    This reverts commit 48ee40110aac6914d3a8646c5ca4564ed473406d.
    
    Change-Id: I6c688bc6261add5d2534e2c3f1d12b3e05107489

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2117,13 +2117,6 @@ static ssize_t dapm_widget_show_codec(struct snd_soc_codec *codec, char *buf)
 	int count = 0;
 	char *state = "not set";
 
-	/* card won't be set for the dummy component, as a spot fix
-	 * we're checking for that case specifically here but in future
-	 * we will ensure that the dummy component looks like others.
-	 */
-	if (!cmpnt->card)
-		return 0;
-
 	list_for_each_entry(w, &codec->component.card->widgets, list) {
 		if (w->dapm != &codec->dapm)
 			continue;

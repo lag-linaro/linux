7ab87af7657f4a798abeaf306fe4f35d957134ad
Lee Jones <lee.jones@linaro.org>
2020-07-06 15:54:09 +0100

    ASoC: samsung: pcm: Demote half-documented kerneldoc header
    Ideally the author or someone 'in the know' should come back and
    provide the remainder of the documentation.  Until the kerneldoc
    checkers have been satisfied, let's just demote the header to a
    standard comment block.
    
    Fixes the following W=1 kernel build warning(s):
    
     sound/soc/samsung/pcm.c:127: warning: Function parameter or member 'lock' not described in 's3c_pcm_info'
     sound/soc/samsung/pcm.c:127: warning: Function parameter or member 'sclk_per_fs' not described in 's3c_pcm_info'
     sound/soc/samsung/pcm.c:127: warning: Function parameter or member 'idleclk' not described in 's3c_pcm_info'
     sound/soc/samsung/pcm.c:127: warning: Function parameter or member 'pclk' not described in 's3c_pcm_info'
     sound/soc/samsung/pcm.c:127: warning: Function parameter or member 'cclk' not described in 's3c_pcm_info'
    
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Sangbeom Kim <sbkim73@samsung.com>
    Cc: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Cc: Jaswinder Singh <jassisinghbrar@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- sound/soc/samsung/pcm.c
+++ sound/soc/samsung/pcm.c
@@ -102,7 +102,7 @@
 #define S3C_PCM_FIFOSTAT_RXFIFOFULL		(0x1 << 1)
 #define S3C_PCM_FIFOSTAT_RXFIFOALMSTFULL	(0x1 << 0)
 
-/**
+/*
  * struct s3c_pcm_info - S3C PCM Controller information
  * @dev: The parent device passed to use from the probe.
  * @regs: The pointer to the device register block.

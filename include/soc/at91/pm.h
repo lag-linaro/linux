c8da5ce1ab4a9f791dec694fcdf1a0dba4897e2d
Lee Jones <lee.jones@linaro.org>
2020-07-13 13:15:39 +0100

    arch: arm: mach-at91: pm: Move prototypes to mutually included header
    Both the caller and the supplier's source file should have access to
    the include file containing the prototypes.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/pinctrl/pinctrl-at91.c:1637:6: warning: no previous prototype for ‘at91_pinctrl_gpio_suspend’ [-Wmissing-prototypes]
     1637 | void at91_pinctrl_gpio_suspend(void)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~
     drivers/pinctrl/pinctrl-at91.c:1661:6: warning: no previous prototype for ‘at91_pinctrl_gpio_resume’ [-Wmissing-prototypes]
     1661 | void at91_pinctrl_gpio_resume(void)
     | ^~~~~~~~~~~~~~~~~~~~~~~~
    
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Nicolas Ferre <nicolas.ferre@microchip.com>
    Cc: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Cc: Ludovic Desroches <ludovic.desroches@microchip.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

---
+++ include/soc/at91/pm.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * atmel platform data
+ */
+
+#ifdef CONFIG_PINCTRL_AT91
+void at91_pinctrl_gpio_suspend(void);
+void at91_pinctrl_gpio_resume(void);
+#endif

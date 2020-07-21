f4cef1d4ddf780a50227ff124e5fcf0f8b93941b
JianMin Liu <jian-min.liu@mediatek.com>
2020-07-21 13:36:22 +0800

    NOUPSTREAM: ANDROID: kernel: Core kernel related vendor hooks
      - Add the hook to apply vendor's performance tune for owner
        of rwsem.
    
      - Add the hook for the waiter list of rwsem to allow
        vendor perform waiting queue enhancement
    
      - ANDROID_VENDOR_DATA added to rw_semaphore
    
    [CPNOTE: 27/05/21] Lee: Vendor related code - maintain forever
    
    Squash:
      NOUPSTREAM: ANDROID: workqueue: add vendor hook for wq lockup information
      NOUPSTREAM: ANDROID: printk: add vendor hook for console flush
      NOUPSTREAM: ANDROID: vendor_hooks: Add hook for jiffies updates
      NOUPSTREAM: ANDROID: sched_clock: Add vendor hook to print epoch values
      NOUPSTREAM: ANDROID: sched/cpuset: Add vendor hook to change tasks affinity
      NOUPSTREAM: ANDROID: tracing: Add restricted vendor hooks for preemptirq events
      NOUPSTREAM: ANDROID: ftrace: vendor hook for ftrace dump on oops
      NOUPSTREAM: ANDROID: cgroup: Add vendor hook to the cgroup
      NOUPSTREAM: ANDROID: Add vendor hooks when syscall prctl finished
      NOUPSTREAM: ANDROID: timer: Add vendor hook for timer calc index
      ANDROID: cgroup: Add android_rvh_cgroup_force_kthread_migration
      ANDROID: kernel: Add restricted vendor hook in creds
      ANDROID: module: Add vendor hook
      ANDROID: syscall_check: add vendor hook for bpf syscall
      ANDROID: Re-apply vendor hooks for information of blocked tasks
      ANDROID: Re-apply vendor hooks for rt_mutex information of blocked tasks
      Revert "ANDROID: timer: Add vendor hook for timer calc index"
    
    Bug: 161400830
    Bug: 169374262
    Bug: 165340180
    Bug: 148928265
    Bug: 172945021
    Bug: 174125747
    Bug: 174541725
    Bug: 177387994
    Bug: 181917687
    Bug: 181819699
    Bug: 178758017
    Bug: 184594949
    Bug: 181639260
    Bug: 181639260
    Bug: 191291287
    Bug: 216016261
    
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Change-Id: I007a5e26f3db2adaeaf4e5ccea414ce7abfa83b8
    Signed-off-by: Lee Jones <joneslee@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -11,10 +11,19 @@
 #include <linux/tracepoint.h>
 
 #include <trace/hooks/mpam.h>
+#include <trace/hooks/wqlockup.h>
 #include <trace/hooks/debug.h>
+#include <trace/hooks/printk.h>
+#include <trace/hooks/epoch.h>
+#include <trace/hooks/ufshcd.h>
+#include <trace/hooks/cgroup.h>
+#include <trace/hooks/sys.h>
 #include <trace/hooks/iommu.h>
+#include <trace/hooks/net.h>
 #include <trace/hooks/vmscan.h>
+#include <trace/hooks/creds.h>
 #include <trace/hooks/syscall_check.h>
+#include <trace/hooks/timer.h>
 #include <trace/hooks/fpsimd.h>
 
 /*
@@ -22,10 +31,32 @@
  * associated with them) to allow external modules to probe them.
  */
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mpam_set);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_lockup_pool);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipi_stop);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_hotplug);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_caller_id);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_caller);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_ext_header);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_suspend_epoch_val);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_resume_epoch_val);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_cgroup_attach);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_cgroup_online);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_fill_prdt);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_prepare_command);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_update_sysfs);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_send_command);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_compl_command);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_attach);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_iommu_setup_dma_ops);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ptype_head);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_balance_anon_file_reclaim);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_commit_creds);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_exit_creds);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_override_creds);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_revert_creds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_mmap_file);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_timer_calc_index);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);

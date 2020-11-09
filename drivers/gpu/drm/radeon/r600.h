d10ea284d1bde672d4dc8113cb183eed6d95a722
Lee Jones <lee.jones@linaro.org>
2020-11-09 13:43:38 +0000

    drm/radeon/radeon_audio: Move 'r600_*' prototypes into shared header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/radeon/r600_hdmi.c:177:6: warning: no previous prototype for ‘r600_hdmi_update_acr’ [-Wmissing-prototypes]
     177 | void r600_hdmi_update_acr(struct drm_encoder *encoder, long offset,
     | ^~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/r600_hdmi.c:217:6: warning: no previous prototype for ‘r600_set_avi_packet’ [-Wmissing-prototypes]
     217 | void r600_set_avi_packet(struct radeon_device *rdev, u32 offset,
     | ^~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/r600_hdmi.c:314:6: warning: no previous prototype for ‘r600_hdmi_audio_set_dto’ [-Wmissing-prototypes]
     314 | void r600_hdmi_audio_set_dto(struct radeon_device *rdev,
     | ^~~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/r600_hdmi.c:340:6: warning: no previous prototype for ‘r600_set_vbi_packet’ [-Wmissing-prototypes]
     340 | void r600_set_vbi_packet(struct drm_encoder *encoder, u32 offset)
     | ^~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/r600_hdmi.c:351:6: warning: no previous prototype for ‘r600_set_audio_packet’ [-Wmissing-prototypes]
     351 | void r600_set_audio_packet(struct drm_encoder *encoder, u32 offset)
     | ^~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/r600_hdmi.c:393:6: warning: no previous prototype for ‘r600_set_mute’ [-Wmissing-prototypes]
     393 | void r600_set_mute(struct drm_encoder *encoder, u32 offset, bool mute)
     | ^~~~~~~~~~~~~
     drivers/gpu/drm/radeon/r600_hdmi.c:469:6: warning: no previous prototype for ‘r600_hdmi_enable’ [-Wmissing-prototypes]
     469 | void r600_hdmi_enable(struct drm_encoder *encoder, bool enable)
     | ^~~~~~~~~~~~~~~~
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/gpu/drm/radeon/r600.h
+++ drivers/gpu/drm/radeon/r600.h
@@ -28,10 +28,26 @@
 #ifndef __R600_H__
 #define __R600_H__
 
+struct r600_audio_pin;
+struct radeon_crtc;
 struct radeon_device;
+struct radeon_hdmi_acr;
 
 u32 r600_gpu_check_soft_reset(struct radeon_device *rdev);
 int r600_ih_ring_alloc(struct radeon_device *rdev);
 void r600_ih_ring_fini(struct radeon_device *rdev);
+
+void r600_audio_enable(struct radeon_device *rdev, struct r600_audio_pin *pin,
+		       u8 enable_mask);
+void r600_set_audio_packet(struct drm_encoder *encoder, u32 offset);
+void r600_set_mute(struct drm_encoder *encoder, u32 offset, bool mute);
+void r600_hdmi_audio_set_dto(struct radeon_device *rdev,
+			     struct radeon_crtc *crtc, unsigned int clock);
+void r600_set_avi_packet(struct radeon_device *rdev, u32 offset,
+			 unsigned char *buffer, size_t size);
+void r600_hdmi_update_acr(struct drm_encoder *encoder, long offset,
+			  const struct radeon_hdmi_acr *acr);
+void r600_set_vbi_packet(struct drm_encoder *encoder, u32 offset);
+void r600_hdmi_enable(struct drm_encoder *encoder, bool enable);
 
 #endif				/* __R600_H__ */

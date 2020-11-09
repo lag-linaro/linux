2230729ecb339023e07a71f65e1ebe5f0ba588e9
Lee Jones <lee.jones@linaro.org>
2020-11-09 13:59:37 +0000

    drm/radeon/evergreend: Move 'evergreen_*' and 'sumo_' prototypes out to shared location
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/radeon/evergreen.c:1281:6: warning: no previous prototype for ‘evergreen_fix_pci_max_read_req_size’ [-Wmissing-prototypes]
     1281 | void evergreen_fix_pci_max_read_req_size(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:2664:6: warning: no previous prototype for ‘evergreen_mc_stop’ [-Wmissing-prototypes]
     2664 | void evergreen_mc_stop(struct radeon_device *rdev, struct evergreen_mc_save *save)
     | ^~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:2762:6: warning: no previous prototype for ‘evergreen_mc_resume’ [-Wmissing-prototypes]
     2762 | void evergreen_mc_resume(struct radeon_device *rdev, struct evergreen_mc_save *save)
     | ^~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:2850:6: warning: no previous prototype for ‘evergreen_mc_program’ [-Wmissing-prototypes]
     2850 | void evergreen_mc_program(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:3710:5: warning: no previous prototype for ‘evergreen_mc_init’ [-Wmissing-prototypes]
     3710 | int evergreen_mc_init(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:3769:6: warning: no previous prototype for ‘evergreen_print_gpu_status_regs’ [-Wmissing-prototypes]
     3769 | void evergreen_print_gpu_status_regs(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:3797:6: warning: no previous prototype for ‘evergreen_is_display_hung’ [-Wmissing-prototypes]
     3797 | bool evergreen_is_display_hung(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:4009:6: warning: no previous prototype for ‘evergreen_gpu_pci_config_reset’ [-Wmissing-prototypes]
     4009 | void evergreen_gpu_pci_config_reset(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:4110:6: warning: no previous prototype for ‘sumo_rlc_fini’ [-Wmissing-prototypes]
     4110 | void sumo_rlc_fini(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:4153:5: warning: no previous prototype for ‘sumo_rlc_init’ [-Wmissing-prototypes]
     4153 | int sumo_rlc_init(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:4381:5: warning: no previous prototype for ‘evergreen_rlc_resume’ [-Wmissing-prototypes]
     4381 | int evergreen_rlc_resume(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~
     drivers/gpu/drm/radeon/evergreen.c:4670:6: warning: no previous prototype for ‘evergreen_irq_suspend’ [-Wmissing-prototypes]
     4670 | void evergreen_irq_suspend(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~
    
    NB: Lots more of these - snipped for brevity
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/gpu/drm/radeon/evergreend.h
+++ drivers/gpu/drm/radeon/evergreend.h
@@ -24,6 +24,25 @@
 #ifndef EVERGREEND_H
 #define EVERGREEND_H
 
+struct evergreen_mc_save;
+
+bool evergreen_is_display_hung(struct radeon_device *rdev);
+void evergreen_print_gpu_status_regs(struct radeon_device *rdev);
+void evergreen_mc_stop(struct radeon_device *rdev, struct evergreen_mc_save *save);
+void evergreen_mc_resume(struct radeon_device *rdev, struct evergreen_mc_save *save);
+int evergreen_mc_wait_for_idle(struct radeon_device *rdev);
+void evergreen_mc_program(struct radeon_device *rdev);
+void evergreen_irq_suspend(struct radeon_device *rdev);
+int evergreen_mc_init(struct radeon_device *rdev);
+void evergreen_fix_pci_max_read_req_size(struct radeon_device *rdev);
+void evergreen_pcie_gen2_enable(struct radeon_device *rdev);
+void evergreen_program_aspm(struct radeon_device *rdev);
+void sumo_rlc_fini(struct radeon_device *rdev);
+int sumo_rlc_init(struct radeon_device *rdev);
+void evergreen_gpu_pci_config_reset(struct radeon_device *rdev);
+u32 evergreen_get_number_of_dram_channels(struct radeon_device *rdev);
+void evergreen_print_gpu_status_regs(struct radeon_device *rdev);
+
 #define EVERGREEN_MAX_SH_GPRS           256
 #define EVERGREEN_MAX_TEMP_GPRS         16
 #define EVERGREEN_MAX_SH_THREADS        256

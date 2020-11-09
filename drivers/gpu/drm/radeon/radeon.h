1d4dd3090bb1ffcdbe3b0693404594e087308bfc
Lee Jones <lee.jones@linaro.org>
2020-11-09 12:33:19 +0000

    drm/radeon/radeon: Move 'radeon_add_legacy_encoder's prototype to shared location
    Fixes the following W=1 kernel build warning(s):
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Cc: linux-media@vger.kernel.org
    Cc: linaro-mm-sig@lists.linaro.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -2818,6 +2818,8 @@ extern int radeon_cs_parser_init(struct radeon_cs_parser *p, void *data);
 extern void radeon_legacy_set_clock_gating(struct radeon_device *rdev, int enable);
 extern void radeon_legacy_backlight_init(struct radeon_encoder *radeon_encoder,
 					 struct drm_connector *drm_connector);
+extern void radeon_add_legacy_encoder(struct drm_device *dev, uint32_t encoder_enum,
+				      uint32_t supported_device);
 extern void radeon_atom_set_clock_gating(struct radeon_device *rdev, int enable);
 extern void radeon_ttm_placement_from_domain(struct radeon_bo *rbo, u32 domain);
 extern bool radeon_ttm_bo_is_radeon_bo(struct ttm_buffer_object *bo);

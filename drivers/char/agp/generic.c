382457e9b5b01349bf1f7612998381c556c85c63
Lee Jones <lee.jones@linaro.org>
2021-03-31 13:00:23 +0100

    char: agp: generic: Place braces around optimised out function in if()
    Fixes the following W=1 kernel build warning(s):
    
     drivers/char/agp/generic.c: In function ‘agp_generic_destroy_page’:
     drivers/char/agp/generic.c:1265:28: warning: suggest braces around empty body in an ‘if’ statement [-Wempty-body]
    
    Cc: David Airlie <airlied@linux.ie>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/char/agp/generic.c
+++ drivers/char/agp/generic.c
@@ -1261,8 +1261,9 @@ void agp_generic_destroy_page(struct page *page, int flags)
 	if (page == NULL)
 		return;
 
-	if (flags & AGP_PAGE_DESTROY_UNMAP)
+	if (flags & AGP_PAGE_DESTROY_UNMAP) {
 		unmap_page_from_agp(page);
+	}
 
 	if (flags & AGP_PAGE_DESTROY_FREE) {
 		put_page(page);

29f62434539f67380b570f754d0f0dcbc0f5a87c
Muchun Song <songmuchun@bytedance.com>
2022-04-01 11:28:36 -0700

    BACKPORT: mm: kfence: fix objcgs vector allocation
    If the kfence object is allocated to be used for objects vector, then
    this slot of the pool eventually being occupied permanently since the
    vector is never freed.  The solutions could be (1) freeing vector when
    the kfence object is freed or (2) allocating all vectors statically.
    
    Since the memory consumption of object vectors is low, it is better to
    chose (2) to fix the issue and it is also can reduce overhead of vectors
    allocating in the future.
    
    Bug: 254441685
    Link: https://lkml.kernel.org/r/20220328132843.16624-1-songmuchun@bytedance.com
    Fixes: d3fb45f370d9 ("mm, kfence: insert KFENCE hooks for SLAB")
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Reviewed-by: Marco Elver <elver@google.com>
    Reviewed-by: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Xiongchun Duan <duanxiongchun@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 8f0b36497303487d5a32c75789c77859cc2ee895)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I9f139018f4f3c4fd23d7833571862032fcb1fc11

--- mm/kfence/kfence.h
+++ mm/kfence/kfence.h
@@ -85,6 +85,9 @@ struct kfence_metadata {
 	/* Allocation and free stack information. */
 	struct kfence_track alloc_track;
 	struct kfence_track free_track;
+#ifdef CONFIG_MEMCG
+	struct obj_cgroup *objcg;
+#endif
 };
 
 extern struct kfence_metadata kfence_metadata[CONFIG_KFENCE_NUM_OBJECTS];

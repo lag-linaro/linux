a2551d7bee6d645ad8fc9ca132dc8e8b26504d70
Lee Jones <lee@kernel.org>
2023-08-22 15:04:08 +0000

    drm/scheduler/sched_main: Provide short description of missing param 'result'
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/scheduler/sched_main.c:266: warning: Function parameter or member 'result' not described in 'drm_sched_job_done'
    
    Signed-off-by: Lee Jones <lee@kernel.org>
    ---
    Cc: Luben Tuikov <luben.tuikov@amd.com>
    Cc: David Airlie <airlied@gmail.com>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: dri-devel@lists.freedesktop.org
    Cc: linux-media@vger.kernel.org
    Cc: linaro-mm-sig@lists.linaro.org

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -259,6 +259,7 @@ drm_sched_rq_select_entity_fifo(struct drm_sched_rq *rq)
 /**
  * drm_sched_job_done - complete a job
  * @s_job: pointer to the job which is done
+ * @result: fence error to forward and set
  *
  * Finish the job's fence and wake up the worker thread.
  */

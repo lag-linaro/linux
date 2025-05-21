16779d52039f50e6dd8dce8a1cccb6810508ef8a
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-05-21 14:45:16 +0000

    UPSTREAM: af_unix: Allocate struct unix_edge for each inflight AF_UNIX fd.
    commit 29b64e354029cfcf1eea4d91b146c7b769305930 upstream.
    
    As with the previous patch, we preallocate to skb's scm_fp_list an
    array of struct unix_edge in the number of inflight AF_UNIX fds.
    
    There we just preallocate memory and do not use immediately because
    sendmsg() could fail after this point.  The actual use will be in
    the next patch.
    
    When we queue skb with inflight edges, we will set the inflight
    socket's unix_sock as unix_edge->predecessor and the receiver's
    unix_sock as successor, and then we will link the edge to the
    inflight socket's unix_vertex.edges.
    
    Note that we set NULL to cloned scm_fp_list.edges in scm_fp_dup()
    so that MSG_PEEK does not change the shape of the directed graph.
    
    Bug: 404256079
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Link: https://lore.kernel.org/r/20240325202425.60930-3-kuniyu@amazon.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Lee Jones <lee@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 4fc7df1c6da14aef3ad9d2ea39565c020ec09aaa)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I9b0d639f1ac8fb3b9c032aebcca6d5bb59e3fc79

--- net/unix/garbage.c
+++ net/unix/garbage.c
@@ -129,6 +129,11 @@ int unix_prepare_fpl(struct scm_fp_list *fpl)
 		list_add(&vertex->entry, &fpl_ext->vertices);
 	}
 
+	fpl->edges = kvmalloc_array(fpl->count_unix, sizeof(*fpl->edges),
+				    GFP_KERNEL_ACCOUNT);
+	if (!fpl->edges)
+		goto err;
+
 	return 0;
 
 err:
@@ -138,6 +143,7 @@ err:
 
 void unix_destroy_fpl(struct scm_fp_list *fpl)
 {
+	kvfree(fpl->edges);
 	unix_free_vertices(fpl);
 }
 
